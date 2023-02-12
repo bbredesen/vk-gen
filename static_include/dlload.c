9b707af8f6096833596622af7ddf39f9716b3934
Ben Bredesen <ben.bredesen@gmail.com>
2023-02-12 13:00:17 -0600

    Support up to 12 params to VK

--- static_include/dlload.c
+++ static_include/dlload.c
@@ -54,6 +54,7 @@ typedef uint32_t (*vkEnumerateInstanceVersion_func)(uint32_t*);
 typedef size_t (*vkGeneric_func3)(uintptr_t, uintptr_t, uintptr_t);
 typedef size_t (*vkGeneric_func6)(uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 typedef size_t (*vkGeneric_func9)(uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t);
+typedef size_t (*vkGeneric_func12)(uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 
 size_t Trampoline3(void *symbol, size_t p0, size_t p1, size_t p2) {
 	return ((vkGeneric_func3) symbol)(p0, p1, p2);
@@ -65,3 +66,6 @@ size_t Trampoline9(void *symbol, size_t p0, size_t p1, size_t p2, size_t p3, siz
 	return ((vkGeneric_func9) symbol)(p0, p1, p2, p3, p4, p5, p6, p7, p8);
 }
 
+size_t Trampoline12(void *symbol, uintptr_t p0, uintptr_t p1, uintptr_t p2, uintptr_t p3, uintptr_t p4, uintptr_t p5, uintptr_t p6, uintptr_t p7, uintptr_t p8, uintptr_t p9, uintptr_t p10, uintptr_t p11) {
+		return ((vkGeneric_func12) symbol)(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11);
+}
